527d67482fc912848aacc9ad01704248abcca338
plausibility <chris@gibsonsec.org>
2013-04-25 11:59:32 +1000

    Cleaned up significantly through use of argparse module

--- blc_hash.py
+++ blc_hash.py
@@ -6,36 +6,34 @@ import random
 import time
 import string
 import hashlib
+import argparse
 
 ###
-thread_count = 5
 thread_list = []
-
-debug = False
-quiet = False
-
-# TODO: submit to sqlite3 database
-possible = []
-upper_bound = 1.5 * (10**6)
-
 charmap = string.lowercase + string.uppercase + string.digits
 ###
 
-if len(sys.argv) < 2:
-    print "Invalid arguments: <threads>"
-    sys.exit(1)
-try:
-    thread_count = int(sys.argv[1])
-    for arg in sys.argv:
-        if arg.lower() in ('-d', '--debug'):
-            debug = True
-            break
-        elif arg.lower() in ('-q', '--quiet'):
-            quiet = True
-            break
-except:
-    print "Error parsing arguments: <threads>"
-    sys.exit(1)
+parser = argparse.ArgumentParser(description="Mine your way to riches and glory (or something..) with BlooCoins.")
+parser.add_argument("-t", "--threads",
+                    help="The amount of worker threads",
+                    type=int,
+                    default=5)
+parser.add_argument("-q", "--queue",
+                    help="The queue size to work through",
+                    type=int,
+                    default=50)
+parser.add_argument("-u", "--upper-bound",
+                    help="The upper bound at which we abandon the job (default: %(default)s)",
+                    type=int,
+                    default=1.5*(10**6))
+parser.add_argument("--debug",
+                    help="Spam the terminal with debugging text.",
+                    action="store_true")
+parser.add_argument("-d", "--difficulty",
+                    help="The difficulty as reported by the server",
+                    type=int,
+                    default=7)
+args = parser.parse_args()
 
 start_time = time.time()
  
@@ -45,13 +43,13 @@ def start_string():
 q = Queue.Queue()
 
 def worker():
-    difficulty = 7
+    difficulty = args.difficulty
     num = 0
-    buf = "0"*7#chr(0) * difficulty # (7 == difficulty from server)
+    buf = "0" * difficulty # (7 == difficulty from server atm)
     while True:
         work = q.get()
         num = 0
-        sys.stdout.write("Working on: " + work + "\n")
+        sys.stdout.write("[{0}] started work".format(work) + "\n")
         sys.stdout.flush()
         # Layered while loops, because... fuck it.
         while True:
@@ -59,28 +57,29 @@ def worker():
             if work_hash[0:difficulty] == buf:
                 with open(work + str(num) + '.blc', 'w') as f:
                     f.write(work_hash)
-                sys.stdout.write("Hit the jackpot // " + work + str(num) + " -> " + work_hash + "\n")
+                sys.stdout.write("[{0}] Hit the jackpot! // {0}{1} -> {2}".format(work, num, work_hash) + "\n")
                 sys.stdout.flush()
                 break
             num += 1
-            if num >= upper_bound:
-                if debug:
-                    sys.stdout.write("hit upper bound: " + str(num) + " @" + work + "\n")
-                    sys.stdout.flush()
+            if num >= args.upper_bound:
+                if args.debug:
+                    sys.stdout.write("[{0}] hit upper bound: {1}".format(work, num) + "\n")
+                sys.stdout.write("[{0}] no success".format(work) + "\n")
+                sys.stdout.flush()
                 break
         q.task_done()
  
-for i in range(thread_count):
+for i in range(args.threads):
     t = threading.Thread(target=worker)
     thread_list.append(t)
     t.daemon = True
     t.start()
 
-for i in range(0, 50):
+for i in range(0, args.queue):
     q.put(start_string())
 
 q.join()
 
 finish_time = time.time()
 
-print finish_time - start_time, "!", "TADA!"
+print "Finished in:", finish_time - start_time, "-", "TADA!"
